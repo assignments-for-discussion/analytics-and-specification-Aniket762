342440d7e630a18d2a79486d92212c7c214c8845
Aniket Pal <aniketindian8@gmail.com>
2021-12-06 14:40:43 +0530

    Fix: Arrow function used to write more optimzied code 🚀

--- average.js
+++ average.js
@@ -1,7 +1,5 @@
 function average(numbers) {
-  const naNumbers = numbers.filter(function (num) {
-    return !Number.isNaN(num);
-  });
+  const naNumbers = numbers.filter((num) => !Number.isNaN(num));
 
   return naNumbers.reduce((p, c) => p + c, 0) / naNumbers.length;
 }
