2b4293e3052f2a1ae995580e41a2747e49817889
Aniket Pal <aniketindian8@gmail.com>
2021-12-08 14:50:33 +0530

    Enhacement: Deleted comments to keep uniformity

--- average.js
+++ average.js
@@ -1,20 +1,15 @@
 function filterOutliers(numbers) {
-  // Creating a copy of the array
   var values = numbers.concat();
 
-  // Sorting the array
   values.sort((a, b) => a - b);
 
-  // Finding outlier thresholds dynamically
   var q1 = values[Math.floor(values.length / 4)];
   var q3 = values[Math.ceil(values.length * (3 / 4)) - 1];
   var iqr = q3 - q1;
 
-  // Setting the minimum and maximum values
   var maxValue = q3 + iqr * 1.5;
   var minValue = q1 - iqr * 1.5;
 
-  // Then filter anything beyond or beneath these values.
   var filteredValues = values.filter((x) => x <= maxValue && x >= minValue);
 
   return filteredValues;
