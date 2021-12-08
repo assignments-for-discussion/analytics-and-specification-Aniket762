c835a603cc2f5929346e231e13f24eace98ac005
Aniket Pal <aniketindian8@gmail.com>
2021-12-08 18:48:03 +0530

    Fix: Commented out prev code to fix workflow

--- average.js
+++ average.js
@@ -21,22 +21,22 @@ function filterOutlierWithSd(numbers) {
   return filteredValues;
 }
 
-function filterOutlierWithIQR(numbers) {
-  var values = numbers.concat();
+// function filterOutlierWithIQR(numbers) {
+//   var values = numbers.concat();
 
-  values.sort((a, b) => a - b);
+//   values.sort((a, b) => a - b);
 
-  var firstQuartile = values[Math.floor(values.length / 4)];
-  var thirdQuartile = values[Math.ceil(values.length * (3 / 4)) - 1];
-  var interQuartileRange = thirdQuartile - firstQuartile;
+//   var firstQuartile = values[Math.floor(values.length / 4)];
+//   var thirdQuartile = values[Math.ceil(values.length * (3 / 4)) - 1];
+//   var interQuartileRange = thirdQuartile - firstQuartile;
 
-  var maxValue = thirdQuartile + interQuartileRange * 1.5;
-  var minValue = firstQuartile - interQuartileRange * 1.5;
+//   var maxValue = thirdQuartile + interQuartileRange * 1.5;
+//   var minValue = firstQuartile - interQuartileRange * 1.5;
 
-  var filteredValues = values.filter((x) => x <= maxValue && x >= minValue);
+//   var filteredValues = values.filter((x) => x <= maxValue && x >= minValue);
 
-  return filteredValues;
-}
+//   return filteredValues;
+// }
 
 function average(numbers) {
   if (numbers.length == 0) {
